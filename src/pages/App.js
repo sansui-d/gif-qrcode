43397774bc2065933f5e12d7ca1a676bc45ac8c8
dengchuandong <sansui1021@163.com>
2023-10-24 23:29:22 +0800

    init

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.less';
+
+function App() {
+
+  return (
+    <div className="gif-qrcode">
+      <div className='gif-qrcode-content'></div>
+    </div>
+  );
+}
+
+export default App;
